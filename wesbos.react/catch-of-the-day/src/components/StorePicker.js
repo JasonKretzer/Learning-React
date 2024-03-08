31f35359a03d7bcfa610aa7ffe78c432e58a9315
Jason Kretzer <JasonKretzer@user.noreply.github.com>
2024-03-08 13:09:24 -0500

    Added some notes on the very barebones basics of state in react, added a way to add a bunch of fish the state of the App component, implemented some onClick handlers

--- wesbos.react/catch-of-the-day/src/components/StorePicker.js
+++ wesbos.react/catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // to be used with a ref in the input element below
+  myInput = React.createRef();
+
+  // do it this way to make `this` available otherwise you have to make
+  // a constructor and do this: this.goToStore = this.goToStore.bind(this)
+  // this is called a property
+  goToStore = (event) => {
+    event.preventDefault();
+    // the first value is from React which gets the element
+    // the second value is called on that
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
         <button type="submit">Visit Store</button>
       </form>
     )
